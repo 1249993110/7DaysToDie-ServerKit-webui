5b33ef2590b613d0d257d9b204cdb6cd600c3911
IceCoffee <1249993110@qq.com>
2024-09-13 09:54:02 +0800

    Add colored chat API endpoints

---
+++ src/api/coloredChat.js
@@ -0,0 +1,49 @@
+
+
+/**
+ * getColoredChat
+ * @returns
+ */
+export const getColoredChat = () => {
+    return http.get('/ColoredChat');
+};
+
+/**
+ * getColoredChatById
+ * @returns
+ */
+export const getColoredChatById = (id) => {
+    return http.get('/ColoredChat/' + id);
+};
+
+/**
+ * addColoredChat
+ * @returns
+ */
+export const addColoredChat = (data) => {
+    return http.post('/ColoredChat', data);
+};
+
+/**
+ * deleteColoredChatById
+ * @returns
+ */
+export const deleteColoredChatById = (id) => {
+    return http.delete('/ColoredChat/' + id);
+};
+
+/**
+ * deleteColoredChatByIds
+ * @returns
+ */
+export const deleteColoredChatByIds = (params) => {
+    return http.delete('/ColoredChat', { params: params });
+};
+
+/**
+ * updateColoredChat
+ * @returns
+ */
+export const updateColoredChat = (id, data) => {
+    return http.put('/ColoredChat/' + id, data);
+};
