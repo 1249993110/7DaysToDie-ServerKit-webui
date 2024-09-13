52955c63ba331126aa551abe9db7e058e7d14d37
IceCoffee <1249993110@qq.com>
2024-09-13 16:15:03 +0800

    Refactor colored chat table in Management view

--- src/views/ColoredChat/Management/index.vue
+++ src/views/ColoredChat/Management/index.vue
@@ -17,8 +17,22 @@
         >
             <template #columns>
                 <el-table-column prop="id" :label="t('views.coloredChat.tableHeader.playerId')" sortable> </el-table-column>
-                <el-table-column prop="nameColor" :label="t('views.coloredChat.tableHeader.nameColor')" sortable> </el-table-column>
-                <el-table-column prop="textColor" :label="t('views.coloredChat.tableHeader.textColor')" sortable> </el-table-column>
+                <el-table-column prop="nameColor" :label="t('views.coloredChat.tableHeader.nameColor')" sortable>
+                    <template #default="{ row }">
+                        <span class="tableColColor">
+                            <div :style="{ backgroundColor: '#' + row.nameColor }"></div>
+                            {{ row.nameColor }}
+                        </span>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="textColor" :label="t('views.coloredChat.tableHeader.textColor')" sortable>
+                    <template #default="{ row }">
+                        <span class="tableColColor">
+                            <div :style="{ backgroundColor: '#' + row.textColor }"></div>
+                            {{ row.textColor }}
+                        </span>
+                    </template>
+                </el-table-column>
                 <el-table-column prop="createdAt" :label="t('views.coloredChat.tableHeader.createdAt')" sortable> </el-table-column>
                 <el-table-column prop="description" :label="t('views.coloredChat.tableHeader.description')" show-overflow-tooltip> </el-table-column>
             </template>
@@ -52,3 +66,15 @@ const batchDeleteRequest = async (rows) => {
     return await api.deleteColoredChatByIds({ ids: rows.map((i) => i.id) });
 };
 </script>
+
+<style scoped lang="scss">
+.tableColColor {
+    display: flex;
+    align-items: center;
+    div {
+        height: 20px;
+        width: 20px;
+        margin-right: 5px;
+    }
+}
+</style>
