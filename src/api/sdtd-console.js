ef8044166766b2fde2533ddebf9bc6043830e9e9
IceCoffee <1249993110@qq.com>
2024-09-27 19:33:57 +0800

    Refactor sdtd-console.js to use escaped quotes in command arguments

--- src/api/sdtd-console.js
+++ src/api/sdtd-console.js
@@ -5,27 +5,27 @@ export const sendConsoleCommand = function (command, inMainThread = true) {
 };
 
 export const telePlayer = function (playerIdOrName, target) {
-    return sendConsoleCommand(`tele ${playerIdOrName} ${target}`);
+    return sendConsoleCommand(`tele \"${playerIdOrName}\" \"${target}\"`);
 };
 
 export const giveItem = function (playerIdOrName, itemName, count, quality = 0, durability = 0) {
-    return sendConsoleCommand(`ty-gi ${playerIdOrName} ${itemName} ${count} ${quality} ${durability}`);
+    return sendConsoleCommand(`ty-gi \"${playerIdOrName}\" \"${itemName}\" ${count} ${quality} ${durability}`);
 };
 
 export const spawnEntity = function (playerNameOrEntityId, spawnEntityIdOrName) {
-    return sendConsoleCommand(`se ${playerNameOrEntityId} ${spawnEntityIdOrName}`, true);
+    return sendConsoleCommand(`se \"${playerNameOrEntityId}\" \"${spawnEntityIdOrName}\"`, true);
 };
 
 export const kickPlayer = function (playerIdOrName, reason) {
-    return sendConsoleCommand(`kick ${playerIdOrName} \"${reason}\"`);
+    return sendConsoleCommand(`kick \"${playerIdOrName}\" \"${reason}\"`);
 };
 
 export const addAdmin = function (playerIdOrName, level, displayName) {
-    return sendConsoleCommand(`admin add ${playerIdOrName} ${level} \"${displayName}\"`);
+    return sendConsoleCommand(`admin add \"${playerIdOrName}\" ${level} \"${displayName}\"`);
 };
 
 export const removeAdmin = function (playerIdOrName) {
-    return sendConsoleCommand(`admin remove ${playerIdOrName}`);
+    return sendConsoleCommand(`admin remove \"${playerIdOrName}\"`);
 };
 
 export const changePlayerPoints = function (playerId, count) {
