7e2733103205b5dbaebf2060f2e441cff7d0a500
IceCoffee <1249993110@qq.com>
2024-10-19 19:41:01 +0800

    Refactor ProForm/BtnGroup component to add reverse button order support

---
+++ src/components/ProForm/BtnGroup.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="btn-group">
+        <template v-if="reverse">
+            <el-button v-if="reset?.visible ?? true" :type="reset?.type ?? 'default'" @click="emit('reset')" :icon="reset?.icon">
+                {{ reset?.label ?? t('global.button.reset') }}
+            </el-button>
+            <el-button v-if="submit?.visible ?? true" :type="submit?.type ?? 'primary'" @click="emit('submit')" :icon="submit?.icon">
+                {{ submit?.label ?? t('global.button.submit') }}
+            </el-button>
+            <el-button v-if="cancel?.visible" :type="cancel?.type ?? 'default'" @click="emit('cancel')" :icon="cancel?.icon">
+                {{ cancel?.label ?? t('global.button.cancel') }}
+            </el-button>
+        </template>
+        <template v-else>
+            <el-button v-if="cancel?.visible" :type="cancel?.type ?? 'default'" @click="emit('cancel')" :icon="cancel?.icon">
+                {{ cancel?.label ?? t('global.button.cancel') }}
+            </el-button>
+            <el-button v-if="submit?.visible ?? true" :type="submit?.type ?? 'primary'" @click="emit('submit')" :icon="submit?.icon">
+                {{ submit?.label ?? t('global.button.submit') }}
+            </el-button>
+            <el-button v-if="reset?.visible ?? true" :type="reset?.type ?? 'default'" @click="emit('reset')" :icon="reset?.icon">
+                {{ reset?.label ?? t('global.button.reset') }}
+            </el-button>
+        </template>
+    </div>
+</template>
+
+<script setup>
+const { t } = useI18n();
+
+const props = defineProps({
+    reverse: {
+        type: Boolean,
+        default: false,
+    },
+    position: {
+        type: String,
+        default: 'left',
+    },
+    gutter: {
+        type: [String, Number],
+        default: 12,
+    },
+    cancel: {
+        type: Object,
+        default() {
+            return {
+                type: 'default',
+                visible: false,
+            };
+        },
+    },
+    submit: {
+        type: Object,
+        default() {
+            return {
+                type: 'primary',
+                visible: false,
+            };
+        },
+    },
+    reset: {
+        type: Object,
+        default() {
+            return {
+                type: 'default',
+                visible: false,
+            };
+        },
+    },
+});
+
+const emit = defineEmits(['cancel', 'submit', 'reset']);
+
+const gutter = computed(() => addUnit(props.gutter));
+</script>
+
+<style scoped lang="scss">
+.btn-group {
+    display: flex;
+    justify-content: v-bind(position);
+    .el-button + .el-button {
+        margin-left: v-bind(gutter);
+    }
+}
+</style>
