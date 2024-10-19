8cdf1b19057a56d20ca2188d80e391bad615d483
IceCoffee <1249993110@qq.com>
2024-10-19 19:41:20 +0800

    Add ColSetting component for column settings in ProTable

---
+++ src/components/ProTable/ColSetting.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-drawer v-model="visible" :title="t('components.myTableEx.tableHeader.columnSetting')" :size="size">
+        <div class="table-main">
+            <el-table :data="columns" :border="true">
+                <el-table-column prop="label" align="center" :label="t('components.myTableEx.tableHeader.columnName')" />
+                <el-table-column v-slot="scope" prop="visible" align="center" :label="t('components.myTableEx.tableHeader.display')">
+                    <el-switch v-model="scope.row.visible"></el-switch>
+                </el-table-column>
+            </el-table>
+        </div>
+    </el-drawer>
+</template>
+
+<script setup>
+const { t } = useI18n();
+
+const props = defineProps({
+    columns: {
+        type: Array,
+        required: true,
+    },
+    size: {
+        type: [String, Number],
+        default: 450,
+    },
+});
+
+const visible = ref(false);
+
+const openColSetting = () => {
+    visible.value = true;
+};
+defineExpose({
+    openColSetting,
+});
+
+</script>
