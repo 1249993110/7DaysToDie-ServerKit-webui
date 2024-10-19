b7b883a6edda26acfbab41e5cea7b414cbf6d16a
IceCoffee <1249993110@qq.com>
2024-10-19 19:41:28 +0800

    Refactor ProTable component to add search functionality and toolbar support

---
+++ src/components/ProTable/index.vue
@@ -0,0 +1,324 @@
+<template>
+    <div class="pro-table">
+        <el-card class="search-card" v-loading="loading" shadow="always" v-if="search" v-show="searchVisible">
+            <slot name="search">
+                <ProForm ref="searchRef" :request="getTableData" label-position="right" :model="searchModel" v-bind="search" />
+            </slot>
+        </el-card>
+        <el-card class="table-card" v-loading="loading" shadow="always">
+            <slot name="toolbar" :selected-list="selectedList" :selected-list-ids="selectedListIds" :is-selected="isSelected">
+                <Toolbar
+                    v-if="toolbar"
+                    v-bind="toolbar"
+                    :btn-label-visible="btnLabelVisible"
+                    :selected-list-ids="selectedListIds"
+                    :selected-list="selectedList"
+                    :is-selected="isSelected"
+                    @add-click="handleAdd"
+                    @batch-delete-click="handleBatchDelete"
+                    @batch-operation-command="handleBatchOperationCommand"
+                    @refresh-click="getTableData"
+                    @setting-click="handleOpenColSetting"
+                    @search-click="searchVisible = !searchVisible"
+                />
+            </slot>
+            <el-table
+                stripe
+                show-overflow-tooltip
+                border
+                v-bind="$attrs"
+                ref="tableRef"
+                :size="tableSize"
+                :data="tableData"
+                :row-key="rowKey"
+                :default-sort="defaultSort"
+                @selection-change="handleSelectionChange"
+                @sort-change="handleSortChange"
+            >
+                <slot />
+                <template v-for="item in columns.filter((i) => i.visible)" :key="item.type ?? item.prop">
+                    <!-- selection || index || expand || operation-->
+                    <el-table-column v-if="item.type && columnTypes.includes(item.type)" v-bind="item">
+                        <template #default="scope">
+                            <!-- expand -->
+                            <template v-if="item.type === 'expand'">
+                                <component v-if="item.render" :is="item.render" v-bind="scope" />
+                                <slot v-else :name="item.type" v-bind="scope" />
+                            </template>
+                            <template v-if="operation && item.type === 'operation'">
+                                <component v-if="item.render" :is="item.render" v-bind="scope" />
+                                <slot v-else :name="item.type" v-bind="scope">
+                                    <el-button v-if="operation.editBtnVisible ?? true" size="small" type="primary" :icon="Edit" @click="handleEdit(scope)">
+                                        <template #default v-if="btnLabelVisible">{{ t('global.button.edit') }}</template>
+                                    </el-button>
+                                    <el-button v-if="operation.deleteBtnVisible ?? true" size="small" type="danger" :icon="Delete" @click="handleDelete(scope)">
+                                        <template #default v-if="btnLabelVisible">{{ t('global.button.delete') }}</template>
+                                    </el-button>
+                                </slot>
+                            </template>
+                        </template>
+                    </el-table-column>
+                    <el-table-column v-else v-bind="item">
+                        <template #header="scope" v-if="item.headerRender">
+                            <component v-if="item.headerRender" :is="item.headerRender" v-bind="scope" />
+                            <slot v-else-if="item.prop && $slots[`${item.prop}Header`]" :name="`${item.prop}Header`" v-bind="scope" />
+                            <span v-else>{{ item.label ?? '' }}</span>
+                        </template>
+                        <template #default="scope">
+                            <component v-if="item.render" :is="item.render" v-bind="scope" />
+                            <slot v-else-if="item.prop && $slots[`${item.prop}Cell`]" :name="`${item.prop}Cell`" v-bind="scope" />
+                            <el-tag v-else-if="item.tag" :type="item.tagType">{{ defaultRenderCell(item, scope) }}</el-tag>
+                            <span v-else>{{ defaultRenderCell(item, scope) }}</span>
+                        </template>
+                    </el-table-column>
+                </template>
+                <template #append v-if="$slots.append">
+                    <slot name="append" />
+                </template>
+                <template #empty v-if="$slots.empty">
+                    <slot name="empty" />
+                </template>
+            </el-table>
+            <slot name="footer">
+                <div class="footer">
+                    <el-radio-group v-model="tableSize">
+                        <el-radio value="small" size="small">{{ t('components.myTableEx.radio.small') }}</el-radio>
+                        <el-radio value="default" size="default">{{ t('components.myTableEx.radio.medium') }}</el-radio>
+                        <el-radio value="large" size="large">{{ t('components.myTableEx.radio.large') }}</el-radio>
+                    </el-radio-group>
+                    <el-pagination
+                        v-if="pagination"
+                        background
+                        :page-sizes="[5, 10, 20, 50, 100]"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :size="tableSize"
+                        @size-change="getTableData"
+                        @current-change="getTableData"
+                        :total="paginationModel.total"
+                        v-model:current-page="paginationModel.pageNumber"
+                        v-model:page-size="paginationModel.pageSize"
+                    >
+                    </el-pagination>
+                </div>
+            </slot>
+        </el-card>
+        <ColSetting :columns="columns" ref="colSettingRef" v-if="toolbar" />
+    </div>
+</template>
+
+<script setup>
+import { Edit, Delete } from '@element-plus/icons-vue';
+import ColSetting from './ColSetting.vue';
+import Toolbar from './Toolbar.vue';
+
+const props = defineProps({
+    columns: {
+        type: Array,
+        required: true,
+    },
+    rowKey: {
+        type: String,
+        default: 'id',
+    },
+    defaultSort: {
+        type: Object,
+    },
+    size: {
+        type: String,
+        default: '',
+    },
+    requestGet: {
+        type: Function,
+    },
+    search: {
+        type: [Boolean, Object],
+        default: true,
+    },
+    toolbar: {
+        type: [Boolean, Object],
+        default: true,
+    },
+    operation: {
+        type: [Boolean, Object],
+        default: {
+            editBtnVisible: true,
+            deleteBtnVisible: true,
+        },
+    },
+    pagination: {
+        type: [Boolean, Object],
+        default: {
+            defaultSize: 20,
+        },
+    },
+    btnLabelVisible: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const emit = defineEmits(['addClick', 'batchDeleteClick', 'batchOperationCommand', 'editClick', 'deleteClick', 'update:size', 'update:pagination']);
+
+const { t } = useI18n();
+
+const loading = ref(false);
+
+const searchRef = ref(null);
+const searchVisible = ref(true);
+
+const tableRef = ref(null);
+const tableData = ref([]);
+const columnTypes = ['selection', 'index', 'expand', 'operation'];
+
+const searchModel = reactive({});
+const sortModel = reactive({
+    sortPorp: props.defaultSort?.prop,
+    sortOrder: props.defaultSort?.order,
+});
+const paginationModel = reactive({
+    pageNumber: 1,
+    pageSize: props.pagination.defaultSize ?? 20,
+    total: 0,
+});
+const requestGetParams = computed(() => ({
+    ...paginationModel,
+    ...searchModel,
+    ...sortModel,
+}));
+
+const getTableData = async () => {
+    loading.value = true;
+    try {
+        const data = await Promise.resolve(props.requestGet(requestGetParams.value));
+        tableData.value = Array.isArray(data.items) ? data.items : data;
+        paginationModel.total = data.total ?? 0;
+    } finally {
+        loading.value = false;
+    }
+};
+
+getTableData();
+
+const defaultRenderCell = (item, scope) => {
+    if (item.formatter) {
+        return item.formatter(scope.row, scope.column);
+    }
+
+    return scope.row[item.prop] ?? '';
+};
+
+const isSelected = ref(false);
+const selectedList = ref([]);
+const selectedListIds = computed(() => selectedList.value.map((item) => item[props.rowKey]));
+const handleSelectionChange = (val) => {
+    selectedList.value = val;
+    isSelected.value = val.length !== 0;
+};
+
+const handleSortChange = async ({ prop, order }) => {
+    sortModel.sortPorp = prop;
+    sortModel.sortOrder = order;
+    await getTableData();
+};
+
+const handleAdd = () => {
+    emit('addClick');
+};
+
+const handleBatchDelete = () => {
+    emit('batchDeleteClick', selectedListIds.value, selectedList.value);
+};
+
+const handleBatchOperationCommand = (command) => {
+    emit('batchOperationCommand', command, selectedListIds.value, selectedList.value);
+};
+
+const columns = reactive([...props.columns]);
+columns.forEach((item) => {
+    if (item.type === 'selection') {
+        item.fixed ??= 'left';
+        item.width ??= 50;
+        item.label ??= '\u2610';
+        item.reserveSelection ??= true;
+        item.align ??= 'center';
+        item.showOverflowTooltip ??= false;
+    } else if (item.type === 'index') {
+        item.fixed ??= 'left';
+        item.width ??= 70;
+        item.label ??= t('components.myTableEx.tableHeader.index');
+        item.align ??= 'center';
+        item.showOverflowTooltip ??= false;
+    } else if (item.type === 'expand') {
+        // item.fixed ??= 'left';
+        item.align ??= 'center';
+        item.showOverflowTooltip ??= false;
+    } else if (item.type === 'operation') {
+        item.fixed ??= 'right';
+        item.minWidth ??= 120;
+        // item.width ??= 180;
+        item.label ??= t('components.myTableEx.tableHeader.operate');
+        item.align ??= 'center';
+        item.showOverflowTooltip ??= false;
+    }
+
+    item.visible ??= true;
+});
+
+const colSettingRef = ref(null);
+const handleOpenColSetting = () => {
+    colSettingRef.value.openColSetting();
+};
+
+const handleEdit = (scope) => {
+    emit('editClick', scope.row);
+};
+
+const handleDelete = async (scope) => {
+    emit('deleteClick', scope.row[props.rowKey], scope.row);
+};
+
+const tableSize = useVModel(props, 'size', emit);
+
+defineExpose({
+    loading: loading,
+    columns: columns,
+    columnTypes: columnTypes,
+    tableData: tableData,
+    requestGet: props.requestGet,
+    requestGetParams: requestGetParams,
+});
+</script>
+
+<style scoped lang="scss">
+.pro-table {
+    height: calc(100vh - 140px);
+    display: flex;
+    flex-direction: column;
+
+    .search-card {
+        margin-top: 20px;
+        background-color: #ffffffaf;
+        overflow: unset;
+    }
+    .table-card {
+        flex-grow: 1;
+        margin-top: 20px;
+        background-color: #ffffffaf;
+        :deep(.el-card__body) {
+            display: flex;
+            flex-direction: column;
+            height: calc(100% - 40px);
+        }
+        .el-table {
+            flex-grow: 1;
+        }
+        .footer {
+            display: flex;
+            margin-top: 16px;
+            .el-pagination {
+                margin-left: auto;
+            }
+        }
+    }
+}
+</style>
