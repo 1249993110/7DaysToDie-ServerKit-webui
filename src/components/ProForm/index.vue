b281dad43bb6edf3e3fc2fa2b1eb405861a8bda0
IceCoffee <1249993110@qq.com>
2024-10-19 19:41:08 +0800

    Refactor ProForm component to add form validation and submission functionality

---
+++ src/components/ProForm/index.vue
@@ -0,0 +1,105 @@
+<template>
+    <el-form :model="model" ref="formRef" status-icon class="pro-form">
+        <el-row :gutter="rowGutter">
+            <el-col v-for="item in fields" :key="item.name" v-bind="colSpan">
+                <el-form-item v-bind="item" :prop="item.name">
+                    <component v-if="item.render" :is="item.render" v-model="model[item.name]" v-bind="item.props" />
+                    <!-- <slot v-else-if="item.type === 'slot'" :name="item.slot" v-model="modelValue[item.name]" v-bind="item.props" /> -->
+                    <el-input v-else-if="item.type === 'input'" v-model="model[item.name]" clearable v-bind="item.props" />
+                    <!-- <component
+                        :is="item.type"
+                        v-model="formModel[item.model]"
+                        :placeholder="item.placeholder"
+                        v-bind="item.options ? { options: item.options } : {}"
+                    ></component> -->
+                </el-form-item>
+            </el-col>
+            <el-col v-if="btnGroup && btnGroup.inline" v-bind="colSpan">
+                <el-form-item v-if="btnGroup" class="footer-btns">
+                    <slot name="btnGroup">
+                        <BtnGroup v-bind="btnGroup" @cancel="emit('cancel')" @submit="handleSubmit" @reset="resetForm" />
+                    </slot>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-form-item v-if="btnGroup && !btnGroup.inline" class="footer-btns">
+            <slot name="btnGroup">
+                <BtnGroup v-bind="btnGroup" @cancel="emit('cancel')" @submit="handleSubmit" @reset="resetForm" />
+            </slot>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script setup>
+import BtnGroup from './BtnGroup.vue';
+
+const { t } = useI18n();
+
+const props = defineProps({
+    rowGutter: {
+        type: Number,
+        default: 20,
+    },
+    colSpan: {
+        type: [Number, Object],
+        default: 12,
+    },
+    fields: {
+        type: Object,
+        required: true,
+    },
+    btnGroup: {
+        type: [Boolean, Object],
+        default: true,
+    },
+    model: {
+        type: Object,
+        required: true,
+    },
+    request: {
+        type: Function,
+        required: true,
+    },
+});
+
+const colSpan = computed(() => {
+    if (typeof props.colSpan === 'number') {
+        return {
+            span: props.colSpan,
+        };
+    }
+    return props.colSpan;
+});
+
+const emit = defineEmits(['cancel', 'submit', 'reset', 'error']);
+
+const formRef = ref(null);
+const handleSubmit = async () => {
+    const loading = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        background: 'rgba(0, 0, 0, 0.7)',
+    });
+    try {
+        await formRef.value.validate();
+        const result = await Promise.resolve(props.request());
+        emit('submit', result);
+    } catch (error) {
+        emit('error', error);
+    } finally {
+        loading.close();
+    }
+};
+const resetForm = () => {
+    formRef.value.resetFields();
+    emit('reset');
+};
+</script>
+
+<style scoped lang="scss">
+.pro-form {
+    .footer-btns {
+        margin-bottom: 0;
+    }
+}
+</style>
