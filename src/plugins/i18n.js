7b0f2e756a09337de8682bcdb4c9cfd43a55cf2e
IceCoffee <1249993110@qq.com>
2024-10-24 13:56:00 +0800

    Refactor i18n.js to remove commented out code

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -39,21 +39,6 @@ export const i18n = createI18n({
     legacy: false,
     messages,
     globalInjection: false, // In <template> can use $t
-    // missing: (locale, key, vm) => {        
-    //     const { t, te, tm, rt } = i18n.global;
-    
-    //     const localeMessages = tm(key);
-    //     if (Object.keys(localeMessages).length === 0) {
-    //         console.warn(`Missing translation for ${key}`);
-    //         return '';
-    //     }
-
-    //     if(localeMessages['']){
-    //         return rt(localeMessages['']);
-    //     }
-
-    //     return '';
-    // },
 });
 
 export default (app) => {
