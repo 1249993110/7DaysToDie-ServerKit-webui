c4688a2a350817ebd067290bfd03aa83ffcbbe9c
IceCoffee <1249993110@qq.com>
2024-10-24 11:51:30 +0800

    Refactor Variables component to display a list of values as tags

---
+++ src/components/MySettingsFormCard/Variables.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="variables">
+        <el-tag v-for="item in values" :key="item" class="tag">{{ '{' + item + '}' }}</el-tag>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    values: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
+
+<style scoped lang="scss">
+.variables {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 4px;
+    .tag {
+        margin-right: 8px;
+    }
+}
+</style>
