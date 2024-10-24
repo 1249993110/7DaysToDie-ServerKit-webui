874fc4c560e9751c7cc528f2a939f4590e6efdb4
IceCoffee <1249993110@qq.com>
2024-10-24 11:13:16 +0800

    Refactor table column height in ColSetting.vue

--- src/components/ProTable/ColSetting.vue
+++ src/components/ProTable/ColSetting.vue
@@ -1,7 +1,7 @@
 <template>
     <el-drawer v-model="visible" :title="t('components.myTableEx.tableHeader.columnSetting')" :size="size">
         <div class="table-main">
-            <el-table :data="columns" :border="true">
+            <el-table :data="columns" :border="true" height="calc(100vh - 128px)">
                 <el-table-column prop="label" align="center" :label="t('components.myTableEx.tableHeader.columnName')" />
                 <el-table-column v-slot="scope" prop="visible" align="center" :label="t('components.myTableEx.tableHeader.display')">
                     <el-switch v-model="scope.row.visible"></el-switch>
