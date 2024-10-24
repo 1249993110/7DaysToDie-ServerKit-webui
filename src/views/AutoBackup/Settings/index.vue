4479d92769aea0c9b640b582798d4e8b1ab46074
IceCoffee <1249993110@qq.com>
2024-10-24 09:08:18 +0800

    Refactor AutoBackup settings page to use MySettingsFormCard component

--- src/views/AutoBackup/Settings/index.vue
+++ src/views/AutoBackup/Settings/index.vue
@@ -1,39 +1,7 @@
 <template>
-    <div class="autobackup-settings">
-        <RouterButton :names="['autobackup.settings', 'autobackup.management']"></RouterButton>
-        <el-card shadow="always" class="card">
-            <el-scrollbar always>
-                <div style="margin-right: 16px">
-                    <el-form :model="formModel" ref="formRef" label-width="250px" status-icon>
-                        <el-form-item :label="t('views.autoBackup.isEnabled')" prop="isEnabled">
-                            <el-switch v-model="formModel.isEnabled" />
-                        </el-form-item>
-                        <el-form-item :label="t('views.autoBackup.interval')" prop="interval" required>
-                            <el-input-number v-model="formModel.interval" :min="10" />
-                        </el-form-item>
-                        <el-form-item :label="t('views.autoBackup.retainedFileCountLimit')" prop="retainedFileCountLimit" required>
-                            <el-input-number v-model="formModel.retainedFileCountLimit" :min="0" />
-                        </el-form-item>
-                        <el-form-item :label="t('views.autoBackup.resetIntervalAfterManualBackup')" prop="resetIntervalAfterManualBackup">
-                            <el-switch v-model="formModel.resetIntervalAfterManualBackup" />
-                        </el-form-item>
-                        <el-form-item :label="t('views.autoBackup.skipIfThereAreNoPlayers')" prop="skipIfThereAreNoPlayers">
-                            <el-switch v-model="formModel.skipIfThereAreNoPlayers" />
-                        </el-form-item>
-                        <el-form-item :label="t('views.autoBackup.autoBackupOnServerStartup')" prop="autoBackupOnServerStartup">
-                            <el-switch v-model="formModel.autoBackupOnServerStartup" />
-                        </el-form-item>
-                        <el-form-item :label="t('views.autoBackup.archiveFolder')" prop="archiveFolder">
-                            <el-input v-model="formModel.archiveFolder" />
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="save">{{ t('global.button.save') }}</el-button>
-                            <el-button type="danger" @click="reset">{{ t('global.button.reset') }}</el-button>
-                        </el-form-item>
-                    </el-form>
-                </div>
-            </el-scrollbar>
-        </el-card>
+    <div>
+        <RouterButton :names="['autobackup.settings', 'autobackup.management']" />
+        <MySettingsFormCard settings-name="AutoBackup" :fields="fields" locale-key-prefix="views.autoBackup" />
     </div>
 </template>
 
@@ -44,48 +12,43 @@ export default {
 </script>
 
 <script setup>
-import * as api from '~/api/settings.js';
-
-const { t } = useI18n();
-
-const formModel = reactive({});
-const formRef = ref();
-
-const localeStore = useLocaleStore();
-const getData = async () => {
-    const data = await api.getSettings('AutoBackup', localeStore.getLanguage());
-    Object.assign(formModel, data);
-};
-getData();
-
-const save = async () => {
-    try {
-        await formRef.value.validate();
-        await api.updateSettings('AutoBackup', formModel);
-        ElMessage.success(t('global.message.saveSuccess'));
-    } catch {}
-};
-
-const reset = async () => {
-    try {
-        if (await myconfirm(t('global.message.resetConfirm'))) {
-            await api.resetSettings('AutoBackup', localeStore.getLanguage());
-            await getData();
-            ElMessage.success(t('global.message.resetSuccess'));
-        }
-    } catch {}
-};
+const fields = [
+    {
+        type: 'switch',
+        name: 'isEnabled',
+    },
+    {
+        type: 'input-number',
+        name: 'interval',
+        required: true,
+        props: {
+            min: 10,
+        },
+    },
+    {
+        type: 'input-number',
+        name: 'retainedFileCountLimit',
+        required: true,
+        props: {
+            min: 0,
+        },
+    },
+    {
+        type: 'switch',
+        name: 'resetIntervalAfterManualBackup',
+    },
+    {
+        type: 'switch',
+        name: 'skipIfThereAreNoPlayers',
+    },
+    {
+        type: 'switch',
+        name: 'autoBackupOnServerStartup',
+    },
+    {
+        type: 'input',
+        name: 'archiveFolder',
+        required: true,
+    },
+];
 </script>
-
-<style scoped lang="scss">
-.autobackup-settings {
-    .card {
-        height: 100%;
-        margin-top: 20px;
-        background-color: #ffffffaf;
-        :deep(.el-card__body) {
-            height: calc(100% - 40px);
-        }
-    }
-}
-</style>
