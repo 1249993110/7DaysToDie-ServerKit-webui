18b87445508881fb48712f4f6c4836ac3b9d7cd3
IceCoffee <1249993110@qq.com>
2024-10-24 09:56:16 +0800

    Refactor searchByKeyword function to support keyword search in data filtering

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,19 @@
 export const addUnit = (value, unit = 'px') => {
     return !Object.is(Number(value), NaN) ? `${value}${unit}` : value;
 };
+
+export const searchByKeyword = (data, keyword, fields = []) => {
+    if (!keyword || !keyword.trim()) {
+        return data;
+    }
+
+    const regex = new RegExp(keyword, 'i');
+
+    return data.filter((item) => {
+        const keysToSearch = fields.length ? fields : Object.keys(item);
+
+        return keysToSearch.some((field) => {
+            return item[field] && regex.test(item[field].toString());
+        });
+    });
+};
