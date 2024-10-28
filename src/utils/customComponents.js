c05d33270704da9f44d729fdca9cd826d0b860c0
IceCoffee <1249993110@qq.com>
2024-10-28 15:53:14 +0800

    Add customComponents to utils

---
+++ src/utils/customComponents.js
@@ -0,0 +1,12 @@
+import MyTextarea from '~/components/MyTextarea/index.vue';
+import Coordinate from '~/components/Coordinate/index.vue';
+import Variables from '~/components/MySettingsFormCard/Variables.vue';
+import GameIcon from '~/components/GameIcon/index.vue';
+
+
+export default customComponents = {
+    'my-textarea': MyTextarea,
+    coordinate: Coordinate,
+    variables: Variables,
+    'game-icon': GameIcon
+};
