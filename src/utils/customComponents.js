a71f6bb87d4fa78954f766da8ddec5074a0f4c30
IceCoffee <1249993110@qq.com>
2024-10-29 15:04:47 +0800

    Add MyColorPicker component to customComponents.js

--- src/utils/customComponents.js
+++ src/utils/customComponents.js
@@ -2,6 +2,7 @@ import MyTextarea from '~/components/MyTextarea/index.vue';
 import Coordinate from '~/components/Coordinate/index.vue';
 import Variables from '~/components/MySettingsFormCard/Variables.vue';
 import GameIcon from '~/components/GameIcon/index.vue';
+import MyColorPicker from '~/components/MyColorPicker/index.vue';
 import ItemBlockSelector from '~/components/ItemBlockSelector/index.vue';
 
 export default {
@@ -9,5 +10,6 @@ export default {
     coordinate: Coordinate,
     Variables: Variables,
     GameIcon: GameIcon,
+    MyColorPicker: MyColorPicker,
     ItemBlockSelector: ItemBlockSelector,
 };
