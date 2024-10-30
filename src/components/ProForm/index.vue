441e467c89f0b9bbca500742c4bc1360c72d3904
IceCoffee <1249993110@qq.com>
2024-10-30 15:27:16 +0800

    Refactor ProForm to apply default values on mount and expose resetFields method

--- src/components/ProForm/index.vue
+++ src/components/ProForm/index.vue
@@ -160,18 +160,18 @@ const resetFields = () => {
     emit('reset');
 };
 
-defineExpose({
-    resetFields,
-});
-
-onBeforeMount(() => {
+const applyDefaultValue = () => {
     for (let i = 0; i < props.fields.length; i++) {
         const item = props.fields[i];
         if (item.default != null && item.name) {
             props.model[item.name] = item.default;
         }
     }
-});
+};
+
+applyDefaultValue();
+
+defineExpose({ resetFields });
 </script>
 
 <style scoped lang="scss">
