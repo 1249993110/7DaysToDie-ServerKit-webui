ccfc873626c173d963bf60805f2fc0478ab2efc9
IceCoffee <1249993110@qq.com>
2024-10-31 15:11:55 +0800

    Refactor AssociatedCommands component to replace AddOrEditCommandList with MyFormDialog and update command handling logic

--- src/components/AssociatedCommands/index.vue
+++ src/components/AssociatedCommands/index.vue
@@ -8,26 +8,26 @@
         <el-table :data="tableData" border height="calc(64vh)" highlight-current-row v-loading="loading" @selection-change="handleSelectionChange">
             <el-table-column type="selection" width="42" />
             <!-- <el-table-column prop="id" label="Id" width="80px"> </el-table-column> -->
-            <el-table-column prop="command" :label="t('views.listManagement.tableHeader.command')" show-overflow-tooltip> </el-table-column>
+            <el-table-column prop="command" :label="t('views.listManagement.tableHeader.command')" show-overflow-tooltip />
             <el-table-column :label="t('views.listManagement.tableHeader.inMainThread')" width="150px" sortable>
                 <template #default="{ row }">
                     {{ `${row.inMainThread ? t('global.true') : t('global.false')}` }}
                 </template>
             </el-table-column>
-            <el-table-column prop="description" :label="t('views.listManagement.tableHeader.description')" show-overflow-tooltip> </el-table-column>
+            <el-table-column prop="description" :label="t('views.listManagement.tableHeader.description')" show-overflow-tooltip />
             <el-table-column :label="t('views.listManagement.operate')" align="center" width="120px">
                 <template #default="{ row }">
                     <el-button type="danger" @click="handleDelete(row)">{{ t('global.button.delete') }}</el-button>
                 </template>
             </el-table-column>
         </el-table>
-        <AddOrEditCommandList v-model="addOrEditCommandListVisible" @on-confirm="handleAddConfirm" :is-add="true" />
+        <MyFormDialog ref="addEditDialogRef" :title="t('global.button.add') + ' ' + t('menus.listManagement.commandList')" :fields="addFormFields" :form-model="addFormModel" :request="requestAdd" @submit="handleAddConfirm" />
         <CommandListSelector v-model="commandListSelectorVisible" @on-select="handleSelectCommandList" :disabled-ids="tableData.map((i) => i.id)" />
     </el-dialog>
 </template>
 
 <script setup>
-import { getCommandById } from '~/api/command-list';
+import * as api from '~/api/command-list';
 import { i18n } from '~/plugins/i18n';
 const { t } = i18n.global;
 
@@ -50,13 +50,13 @@ const handleSelectionChange = (val) => {
     batchDeleteDisabled.value = multipleSelection.length === 0;
 };
 
-const addOrEditCommandListVisible = ref(false);
+const addEditDialogRef = ref(null);
 const handleAdd = () => {
-    addOrEditCommandListVisible.value = true;
+    addEditDialogRef.value.open();
 };
 
 const handleAddConfirm = async (id) => {
-    const cmd = await getCommandById(id);
+    const cmd = await api.getCommandById(id);
     tableData.value.push(cmd);
     emit(
         'edit',
@@ -96,5 +96,36 @@ const handleBatchDelete = () => {
         data.map((i) => i.id)
     );
     tableData.value = data;
+};
+
+const addFormModel = reactive({});
+const addFormFields = computed(() => [
+{
+        type: 'input',
+        name: 'command',
+        label: t('views.listManagement.tableHeader.command'),
+        required: true,
+        props: {
+            type: 'textarea',
+        },
+    },
+    {
+        type: 'switch',
+        name: 'inMainThread',
+        label: t('views.listManagement.tableHeader.inMainThread'),
+        default: true,
+    },
+    {
+        type: 'input',
+        name: 'description',
+        label: t('views.listManagement.tableHeader.description'),
+        props: {
+            type: 'textarea',
+        },
+    },
+]);
+
+const requestAdd = async () => {
+    return await api.addCommand(addFormModel);
 };
 </script>
