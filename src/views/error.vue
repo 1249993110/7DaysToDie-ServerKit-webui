7e864d3827ef263fceb068671b7f2347c3e74f19
IceCoffee <1249993110@qq.com>
2024-12-06 14:57:47 +0800

    add 400 error page component with navigation options

---
+++ src/views/error.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="error-page">
+        <div class="error-code">4<span>0</span>0</div>
+        <div class="error-desc">{{ message }}</div>
+        <div class="error-handle">
+            <router-link to="/">
+                <el-button type="primary" size="large">Go back to Home</el-button>
+            </router-link>
+            <router-link to="/login" class="error-btn">
+                <el-button type="primary" size="large">Go back to login page</el-button>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'error',
+    setup() {
+        const route = useRoute();
+        const message = computed(()=>{
+            return route.query.message;
+        });
+        return {
+            message,
+        };
+    },
+};
+</script>
+
+
+<style scoped>
+.error-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    background: #f3f3f3;
+    box-sizing: border-box;
+}
+.error-code {
+    line-height: 1;
+    font-size: 250px;
+    font-weight: bolder;
+    color: red;
+}
+.error-code span {
+    color: orangered;
+}
+.error-desc {
+    font-size: 30px;
+    color: #777;
+}
+.error-handle {
+    margin-top: 30px;
+    padding-bottom: 200px;
+}
+.error-btn {
+    margin-left: 100px;
+}
+</style>
