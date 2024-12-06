98ba8ddcf2ee10f27e13372ea6c07c64206532ed
IceCoffee <1249993110@qq.com>
2024-12-06 14:57:52 +0800

    add refreshToken function to handle token refresh requests

--- src/api/account.js
+++ src/api/account.js
@@ -8,6 +8,14 @@ export const login = (username, password) => {
     return http.post(loginUrl, { grant_type: 'password', username, password }, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
 };
 
+/**
+ * 登录
+ * @returns
+ */
+export const refreshToken = (refreshToken) => {
+    return http.post(loginUrl, { grant_type: 'refresh_token', refresh_token:refreshToken }, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+};
+
 // /**
 //  * 注销
 //  * @returns
