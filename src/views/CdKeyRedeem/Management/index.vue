e6a049703a8ff2c647cb38491ce6a48af11d73b8
IceCoffee <1249993110@qq.com>
2025-01-09 10:23:06 +0800

    Add CdKey Redeem management and settings views with API integration

---
+++ src/views/CdKeyRedeem/Management/index.vue
@@ -0,0 +1,255 @@
+<template>
+    <div>
+        <RouterButton :names="['cdKeyRedeem.settings', 'cdKeyRedeem.management']" />
+        <MyTable
+            ref="myTableRef"
+            row-key="id"
+            :columns="columns"
+            :model-name="rt(tm('menus.cdKeyRedeem')[''])"
+            :toolbar="toolbar"
+            :search="search"
+            :add-edit-form-fields="addEditFormFields"
+            :add-edit-label-width="150"
+            :request="request"
+        >
+            <template #bindCell="{ row }">
+                <el-button size="small" color="#40e0d0" @click="handleAssociatedItem(row)">{{ t('views.cdKeyRedeem.tableHeader.bindItem') }}</el-button>
+                <el-button size="small" color="#8a2be2" @click="handleAssociatedCommand(row)">{{ t('views.cdKeyRedeem.tableHeader.bindCmd') }}</el-button>
+            </template>
+        </MyTable>
+        <AssociatedItems v-model="associatedItemsVisible" v-model:table-data="associatedData" :loading="associatedLoading" @edit="handleItemsEdit" />
+        <AssociatedCommands v-model="associatedCommandsVisible" v-model:table-data="associatedData" :loading="associatedLoading" @edit="handleCommandsEdit" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'cdKeyRedeem.management',
+};
+</script>
+
+<script setup>
+import * as api from '~/api/cdKey';
+
+const { t, tm, rt } = useI18n();
+
+const columns = computed(() => [
+    {
+        type: 'selection',
+    },
+    {
+        prop: 'key',
+        label: t('views.cdKeyRedeem.tableHeader.key'),
+        width: 320,
+        sortable: 'custom',
+    },
+    {
+        prop: 'createdAt',
+        label: t('views.cdKeyRedeem.tableHeader.createdAt'),
+        width: 160,
+        sortable: 'custom',
+    },
+    {
+        prop: 'redeemCount',
+        label: t('views.cdKeyRedeem.tableHeader.redeemCount'),
+        width: 150,
+        sortable: 'custom',
+    },
+    {
+        prop: 'maxRedeemCount',
+        label: t('views.cdKeyRedeem.tableHeader.maxRedeemCount'),
+        width: 180,
+        sortable: 'custom',
+    },
+    {
+        prop: 'expiryAt',
+        label: t('views.cdKeyRedeem.tableHeader.expiryAt'),
+        sortable: 'custom',
+        width: 160,
+    },
+    {
+        prop: 'description',
+        label: t('views.cdKeyRedeem.tableHeader.description'),
+        sortable: 'custom',
+        minWidth: 150,
+    },
+    {
+        prop: 'bind',
+        label: t('views.cdKeyRedeem.tableHeader.bind'),
+        width: 225,
+        headerAlign: 'center',
+    },
+    {
+        type: 'operation',
+        minWidth: 200,
+    },
+]);
+
+const myTableRef = ref(null);
+const toolbar = computed(() => ({
+    batchOperationItems: [
+        {
+            type: 'export',
+            fileName: rt(tm('menus.cdKeyRedeem')['']),
+        },
+    ],
+}));
+
+const search = computed(() => ({
+    fields: [
+        {
+            type: 'input',
+            name: 'keyword',
+            label: t('global.keyword'),
+            props: {
+                autofocus: true,
+            },
+        },
+    ],
+}));
+
+const newId = ref(0);
+const addEditFormFields = computed(() => [
+    {
+        type: 'input',
+        name: 'key',
+        label: t('views.cdKeyRedeem.tableHeader.key'),
+        required: true,
+    },
+    {
+        type: 'input-number',
+        name: 'redeemCount',
+        label: t('views.cdKeyRedeem.tableHeader.redeemCount'),
+        required: true,
+        default: 0,
+        props: {
+            min: 0,
+            disabled: true,
+        },
+    },
+    {
+        type: 'input-number',
+        name: 'maxRedeemCount',
+        label: t('views.cdKeyRedeem.tableHeader.maxRedeemCount'),
+        required: true,
+        default: -1,
+        props: {
+            min: -1,
+        },
+    },
+    {
+        type: 'date-picker',
+        name: 'expiryAt',
+        label: t('views.cdKeyRedeem.tableHeader.expiryAt'),
+        required: false,
+        props: {
+            type: 'datetime',
+            placeholder: t('global.message.datePickerPlaceholder'),
+        },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        label: t('views.cdKeyRedeem.tableHeader.description'),
+        props: {
+            type: 'textarea',
+        },
+    },
+]);
+
+const requestGet = async (params) => {
+    let data = await api.getCdKeys();
+    if (data.length) {
+        newId.value = data[data.length - 1].id + 1;
+    }
+
+    data = searchByKeyword(data, params.keyword, ['key', 'description']);
+    if (params.sortOrder) {
+        const desc = params.sortOrder === 'descending';
+        const sortPorp = params.sortPorp;
+        data = data.sort((a, b) => {
+            if (desc) {
+                return a[sortPorp] < b[sortPorp] ? 1 : -1;
+            }
+
+            return a[sortPorp] > b[sortPorp] ? 1 : -1;
+        });
+    }
+
+    if (params.pageSize < 0) {
+        return {
+            items: data,
+            total: data.length,
+        };
+    }
+    return {
+        items: data.slice((params.pageNumber - 1) * params.pageSize, params.pageNumber * params.pageSize),
+        total: data.length,
+    };
+};
+
+const requestAdd = async (formModel) => {
+    await api.createCdKey(formModel);
+};
+
+const requestEdit = async (formModel) => {
+    await api.updateCdKey(formModel.id, formModel);
+};
+
+const requestDetele = async (id) => {
+    await api.deleteCdKey(id);
+};
+
+const requestBatchDelete = async (selectedIds) => {
+    await api.deleteCdKeys(selectedIds);
+};
+
+const request = {
+    get: requestGet,
+    add: requestAdd,
+    edit: requestEdit,
+    delete: requestDetele,
+    batchDelete: requestBatchDelete,
+};
+
+const lastClickId = ref(0);
+
+const associatedItemsVisible = ref(false);
+const associatedCommandsVisible = ref(false);
+const associatedData = ref([]);
+const associatedLoading = ref(false);
+
+const handleAssociatedItem = async (row) => {
+    associatedLoading.value = true;
+    lastClickId.value = row.id;
+    associatedData.value = [];
+    associatedItemsVisible.value = true;
+    try {
+        const data = await api.getItemList(row.id);
+        associatedData.value = data;
+    } finally {
+        associatedLoading.value = false;
+    }
+};
+
+const handleItemsEdit = async (ids) => {
+    await api.updateItemList(lastClickId.value, ids);
+};
+
+const handleAssociatedCommand = async (row) => {
+    associatedLoading.value = true;
+    lastClickId.value = row.id;
+    associatedData.value = [];
+    associatedCommandsVisible.value = true;
+    try {
+        const data = await api.getCommandList(row.id);
+        associatedData.value = data;
+    } finally {
+        associatedLoading.value = false;
+    }
+};
+
+const handleCommandsEdit = async (ids) => {
+    await api.updateCommandList(lastClickId.value, ids);
+};
+</script>
---
+++ src/views/CdKeyRedeem/Settings/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <RouterButton :names="['cdKeyRedeem.settings', 'cdKeyRedeem.management']" />
+        <MySettingsFormCard settings-name="CdKeyRedeem" :fields="fields" locale-key-prefix="views.cdKeyRedeem" :label-width="280" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'cdKeyRedeem.settings',
+};
+</script>
+
+<script setup>
+const fields = [
+    {
+        type: 'switch',
+        name: 'isEnabled',
+    },
+    {
+        type: 'Variables',
+        name: 'variables',
+        props: {
+            values: ['CdKeyDescription', 'EntityId', 'PlayerId', 'PlayerName'],
+        },
+    },
+    {
+        type: 'input',
+        name: 'hasAlreadyRedeemedTip',
+        required: true,
+    },
+    {
+        type: 'input',
+        name: 'hasReachedMaxRedemptionLimitTip',
+        required: true,
+    },
+    {
+        type: 'input',
+        name: 'hasRedemptionCodeExpiredTip',
+        required: true,
+    },
+    {
+        type: 'input',
+        name: 'redeemSuccessTip',
+        required: true,
+    },
+];
+</script>
